55b12879d93d0e9881133ccadb977b376e6ad5ab
정민 <90408458+jeonjmin@users.noreply.github.com>
2022-11-28 17:41:20 +0900

    gui수정

--- Assets/week12B_UI_DisplayGameObjectProperties/UI_Locate.cs
+++ Assets/week12B_UI_DisplayGameObjectProperties/UI_Locate.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class UI_Locate : MonoBehaviour
 {
+    public Transform : PosReference;
+
+    Camera TargetCamera;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        TargetCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<TargetCamera>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void DisplayAtPoint()
+    {
+        Vector3 WorldPos = PosReference.position;
+        Vector2 ScreenPos = TargetCamera.WorldToScreenPoint(WorldPos);
+        transform.position = ScreenPos;
     }
 }
